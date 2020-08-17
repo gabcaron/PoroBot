f4e9b8881df86c484a422ea1882e11aa569a2f81
gabcaron <gabin@gabincaron.com>
2020-08-17 15:35:40 +0200

    Ajout rank + Embed message => Ok

--- commands/summoner.js
+++ commands/summoner.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
-const RiotAPI = require('./riot-api.js');
-const axios = require('axios');
-const { Http2ServerRequest } = require('http2');
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const lol = require('./lol-api');
+const https = require('https');
+const Discord = require('discord.js');
 
 module.exports = {
     name: 'summoner',
     description: 'Affiche les informations de base d\'un invocateur donné.',
+    aliases: ['rank', 'player'],
     args: true,
     execute(message, args) {
         if(!args.length) {
@@ -24,19 +22,7 @@ module.exports = {
         }
 
         const apiKey = process.env.RIOT_API_KEY;
-        /*axios.get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=' + apiKey)
-            .then(response => {
-                console.log(response.data);
-                //sumName = response.data.name;
-                //sumLevel = response.data.summonerLevel;
-                //sumId = response.data.id;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        */
         
-        const https = require('https');
         const url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
         const url2 = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/';
         const api_link = '?api_key=' + apiKey;
@@ -60,14 +46,26 @@ module.exports = {
                     res.on('end', () => {
                         body2 = JSON.parse(body2);
                         console.log(body2);
-                        message.channel.send(body.name + ' ' + body2[0].queueType);
+                        
+                        const exampleEmbed = new Discord.MessageEmbed()
+                            .setColor('#0099ff')
+                            .setTitle(body.name)
+                            .setAuthor('PoroBot', 'https://cdn.discordapp.com/avatars/717798439580205106/a9428a35b6e426e5d77a2842e3d2c902.webp', 'https://github.com/gabcaron/PoroBot')
+                            .setDescription('Rank et bien plus !')
+                            .setThumbnail('http://ddragon.leagueoflegends.com/cdn/10.16.1/img/profileicon/' + body.profileIconId + '.png')
+                            .addFields(
+                                { name: '\u200B', value: '\u200B' },
+                                { name: 'Solo/Duo', value: body2[0].tier + ' ' + body2[0].rank, inline: true },
+                                { name: 'Flex', value: body2[1].tier + ' ' + body2[1].rank, inline: true },
+                            )
+                            .setTimestamp()
+                            .setFooter('PoroBot', 'https://cdn.discordapp.com/avatars/717798439580205106/a9428a35b6e426e5d77a2842e3d2c902.webp');
+                        
+                        message.channel.send(exampleEmbed);
                     });
                 });
-                //message.channel.send(body.accountId + ' ' + body.summonerLevel);
             });
         });
-
-        /*message.channel.send(body.title);*/
 
     }
 }
